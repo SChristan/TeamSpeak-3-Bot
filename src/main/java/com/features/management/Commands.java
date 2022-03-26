d6e8fa114b28d65e99a18c75830035c77deb7d9b
Hueter <stefan1234595@gmail.com>
2022-03-26 21:34:56 +0100

    feat: add commands to management bot

---
+++ src/main/java/com/features/management/Commands.java
@@ -0,0 +1,32 @@
+package com.features.management;
+
+import com.TS3.TS3Connection;
+import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent;
+
+public class Commands {
+
+    private static Boolean hasRights() {
+        return true;
+    }
+
+    public static void execute(TextMessageEvent textEvent) {
+        if (textEvent.getMessage().startsWith("!management")) {
+            if (hasRights()) {
+                String[] parts = textEvent.getMessage().split(":");
+
+                if (parts[1].equalsIgnoreCase("reload")) {
+                    Constants.initialize();
+                    ActivityDisplay.updateManagerClients();
+                    ActivityDisplay.updateChannelDescription(Types.IS_MANAGER_AND_SUPPORTER);
+                } else if (parts[1].equalsIgnoreCase("restart")) {
+                    ManagementBot.stop();
+                    ManagementBot.start();
+                } else {
+                    TS3Connection.getApi().sendPrivateMessage(textEvent.getInvokerId(), "Unknown command!");
+                }
+            } else {
+                TS3Connection.getApi().sendPrivateMessage(textEvent.getInvokerId(), "You are not my master!");
+            }
+        }
+    }
+}
