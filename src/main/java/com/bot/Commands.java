b75a65ffd2cede8f138bb5cd474cd0a7837ae47d
Hueter <stefan1234595@gmail.com>
2022-03-27 12:48:40 +0200

    feat: implement hasRights function for Commands in bot, but use management bot access for now

--- src/main/java/com/bot/Commands.java
+++ src/main/java/com/bot/Commands.java
@@ -1,17 +1,26 @@
 package com.bot;
 
 import com.TS3.TS3Connection;
+import com.TS3.TS3Infos;
+import com.features.management.ActivityDisplay;
 import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent;
+import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
 
 public class Commands {
 
-    private static Boolean hasRights() {
-        return true;
+    private static Boolean hasRights(Client client) {
+        for (int group_id : ActivityDisplay.getAuthorizedGroups()) {
+            if (client.isInServerGroup(group_id)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void execute(TextMessageEvent textEvent) {
+        Client client = TS3Infos.getOnlineClients().get(textEvent.getInvokerId());
         if (textEvent.getMessage().startsWith("!bot")) {
-            if (hasRights()) {
+            if (hasRights(client)) {
                 String[] parts = textEvent.getMessage().split(":");
 
                 if (parts[1].equalsIgnoreCase("exit")) {
