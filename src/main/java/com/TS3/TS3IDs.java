03c9e5fab3b2d78a46ad5b449cd9399a25d178ee
Hueter <stefan1234595@gmail.com>
2022-05-11 21:20:22 +0200

    v1.1.0

--- src/main/java/com/TS3/TS3IDs.java
+++ src/main/java/com/TS3/TS3IDs.java
@@ -9,7 +9,7 @@ import com.bot.BotMain;
 
 public class TS3IDs {
     // Code version
-    public static String VERSION = "Version 1.0.4";
+    public static String VERSION = "Version 1.1.0";
 
     // Config
     public static String HOSTNAME;
